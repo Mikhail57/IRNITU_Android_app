7e89bbd5f8a15be80c9d1c053974449b68169839
Mikhail Mustakimov <misha3must@yandex.ru>
2018-09-02 12:43:29 +0800

    Room

---
+++ app/src/main/java/istu/edu/irnitu/entity/Faculty.kt
@@ -0,0 +1,6 @@
+package istu.edu.irnitu.entity
+
+data class Faculty(
+        val title: String,
+        val courses: Map<Int, List<String>>
+)
--- app/src/main/java/istu/edu/irnitu/model/data/IrnituApi.kt
+++ app/src/main/java/istu/edu/irnitu/model/data/IrnituApi.kt
@@ -1,11 +1,20 @@
 package istu.edu.irnitu.model.data
 
 import io.reactivex.Single
+import istu.edu.irnitu.entity.Class
+import istu.edu.irnitu.entity.Faculty
 import istu.edu.irnitu.entity.News
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface IrnituApi {
     @GET("/news")
     fun getNews(@Query("page") page: Int): Single<List<News>>
+
+    @GET("/schedule/groups")
+    fun getGroups(): List<Faculty>
+
+    @GET("/schedule/group/{group}")
+    fun getGroup(@Path("group") group: String): Single<List<Class>>
 }
